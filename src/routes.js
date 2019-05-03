6abc78da33a7f42657e8313dcd0cabe28ed87ecd
IannNascimento <49248138+IannNascimento@users.noreply.github.com>
2019-05-02 21:52:34 -0300

    Primeiro commit

---
+++ src/routes.js
@@ -0,0 +1,12 @@
+import Panels from "views/Components/Panels.jsx";
+
+var dashRoutes = [
+  {
+    path: "/panels",
+    name: "Panels",
+    mini: "P",
+    component: Panels,
+    layout: "/admin"
+  }
+];
+export default dashRoutes;
